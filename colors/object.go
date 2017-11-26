1146c9079731be35a7be55cea19f430e10055d77
gernest <geofreyernest@live.com>
2017-11-26 16:26:24 +0300

    add more color manipulation

--- colors/object.go
+++ colors/object.go
@@ -1,14 +1,18 @@
 package colors
 
-import "bytes"
-import "strconv"
+import (
+	"bytes"
+	"math"
+	"strconv"
+)
 
 type Object struct {
-	R, G, B, A uint8
+	R, G, B, A float64
 	IsRGBA     bool
 }
 
 func FromHex(h string) (Object, error) {
+	factor := 1.0 / 255.0
 	if len(h) == 4 {
 		h = extend(h)
 	}
@@ -36,7 +40,8 @@ func FromHex(h string) (Object, error) {
 	if err != nil {
 		return Object{}, err
 	}
-	return Object{R: uint8(rc), G: uint8(gc), B: uint8(bc)}, nil
+	return Object{R: float64(rc) * factor,
+		G: float64(gc) * factor, B: float64(bc) * factor}, nil
 }
 
 func extend(h string) string {
@@ -77,27 +82,109 @@ func (o Object) String() string {
 }
 
 func (o Object) formatRGBA() string {
-	r := strconv.FormatUint(uint64(o.R), 10)
-	g := strconv.FormatUint(uint64(o.G), 10)
-	b := strconv.FormatUint(uint64(o.B), 10)
-	i := o.A
-	if i == 0 {
-		i = 1
-	}
-	a := strconv.FormatUint(uint64(i), 10)
+	rc, gc, bc, ac := o.rgba()
+	r := strconv.FormatUint(uint64(rc), 10)
+	g := strconv.FormatUint(uint64(gc), 10)
+	b := strconv.FormatUint(uint64(bc), 10)
+	a := strconv.FormatUint(uint64(ac), 10)
 	return "rgba(" + r + "," + g + "," + b + "," + a + ")"
 }
 
 func (o Object) formatRGB() string {
-	r := strconv.FormatUint(uint64(o.R), 10)
-	g := strconv.FormatUint(uint64(o.G), 10)
-	b := strconv.FormatUint(uint64(o.B), 10)
-	return "rgb(" + r + "," + g + "," + b + ")"
+	r, g, b := o.rgb()
+	rc := strconv.FormatUint(uint64(r), 10)
+	gc := strconv.FormatUint(uint64(g), 10)
+	bc := strconv.FormatUint(uint64(b), 10)
+	return "rgb(" + rc + "," + gc + "," + bc + ")"
 }
 
 func (o Object) Hex() string {
-	r := strconv.FormatUint(uint64(o.R), 16)
-	g := strconv.FormatUint(uint64(o.G), 16)
-	b := strconv.FormatUint(uint64(o.B), 16)
+	r := strconv.FormatUint(uint64(o.R*255.0+0.5), 16)
+	g := strconv.FormatUint(uint64(o.G*255.0+0.5), 16)
+	b := strconv.FormatUint(uint64(o.B*255.0+0.5), 16)
 	return "#" + r + g + b
+}
+
+func clamp01(v float64) float64 {
+	return math.Max(0.0, math.Min(v, 1.0))
+}
+
+func (o Object) Clamp() Object {
+	return Object{
+		R: clamp01(o.R),
+		G: clamp01(o.G),
+		B: clamp01(o.B),
+	}
+}
+
+func (o Object) Luminance() float64 {
+	v := [3]float64{}
+	for i, c := range []float64{o.R, o.G, o.B} {
+		c /= 255
+		if c <= 0.03928 {
+			v[i] = c / 12.92
+		} else {
+			v[i] = math.Pow((c+0.055)/1.055, 2.4)
+		}
+	}
+	return 0.2126*v[0] + 0.7152*v[1] + 0.0722*v[2]
+}
+
+func (o Object) rgb() (r, g, b uint8) {
+	r = uint8(o.R*255.0 + 0.5)
+	g = uint8(o.G*255.0 + 0.5)
+	b = uint8(o.B*255.0 + 0.5)
+	return
+}
+
+func (o Object) rgba() (r, g, b, a uint8) {
+	r = uint8(o.R*255.0 + 0.5)
+	g = uint8(o.G*255.0 + 0.5)
+	b = uint8(o.B*255.0 + 0.5)
+	a = 1
+	return
+}
+
+func (o Object) Darken(coe float64) Object {
+	coe = clamp01(coe)
+	return Object{
+		R: darken(o.R, coe),
+		G: darken(o.G, coe),
+		B: darken(o.B, coe),
+	}
+}
+
+func (o Object) Lighten(coe float64) Object {
+	coe = clamp01(coe)
+	return Object{
+		R: lighten(o.R, coe),
+		G: lighten(o.G, coe),
+		B: lighten(o.B, coe),
+	}
+}
+
+func (o Object) Fade(value float64) Object {
+	value = clamp01(value)
+	return Object{
+		R:      o.R,
+		G:      o.G,
+		B:      value,
+		IsRGBA: true,
+	}
+}
+
+func darken(v, coe float64) float64 {
+	return v * (1 - coe)
+}
+
+func lighten(v, coe float64) float64 {
+	return v + (244-v)*coe
+}
+
+func (o Object) Emphasis(coe float64) Object {
+	lum := o.Luminance()
+	if lum > 0.5 {
+		return o.Darken(coe)
+	}
+	return o.Lighten(coe)
 }
