d2f2bef9a2bdf873556ac2e5a2f3098eead737d3
gernest <geofreyernest@live.com>
2017-11-26 12:59:27 +0300

    add color manipulations

---
+++ colors/object.go
@@ -0,0 +1,98 @@
+package colors
+
+import "bytes"
+import "strconv"
+import "fmt"
+
+type Object struct {
+	R, G, B, A uint8
+	IsRGBA     bool
+}
+
+func FromHex(h string) (Object, error) {
+	if len(h) == 4 {
+		h = extend(h)
+	}
+	r, err := substrc(h, 1, 2)
+	if err != nil {
+		return Object{}, err
+	}
+	rc, err := strconv.ParseUint(r, 16, 64)
+	if err != nil {
+		return Object{}, err
+	}
+	g, err := substrc(h, 3, 2)
+	if err != nil {
+		return Object{}, err
+	}
+	gc, err := strconv.ParseUint(g, 16, 64)
+	if err != nil {
+		return Object{}, err
+	}
+	b, err := substrc(h, 5, 2)
+	if err != nil {
+		return Object{}, err
+	}
+	bc, err := strconv.ParseUint(b, 16, 64)
+	if err != nil {
+		return Object{}, err
+	}
+	fmt.Printf("%s , %s , %sc\n", r, g, b)
+	return Object{R: uint8(rc), G: uint8(gc), B: uint8(bc)}, nil
+}
+
+func extend(h string) string {
+	var buf bytes.Buffer
+	buf.WriteRune('#')
+	for i := 1; i < len(h); i++ {
+		buf.WriteByte(h[i])
+		buf.WriteByte(h[i])
+	}
+	return buf.String()
+}
+
+func substrc(s string, start, length int) (string, error) {
+	if start >= len(s) || length <= 0 {
+		return "", nil
+	}
+	r := bytes.NewReader([]byte(s))
+	_, err := r.Seek(int64(start), 0)
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	for i := start; i < start+length; i++ {
+		ch, _, err := r.ReadRune()
+		if err != nil {
+			return "", err
+		}
+		buf.WriteRune(ch)
+	}
+	return buf.String(), nil
+}
+
+func (o Object) String() string {
+	if o.IsRGBA {
+		return o.formatRGBA()
+	}
+	return o.formatRGB()
+}
+
+func (o Object) formatRGBA() string {
+	r := strconv.FormatUint(uint64(o.R), 10)
+	g := strconv.FormatUint(uint64(o.G), 10)
+	b := strconv.FormatUint(uint64(o.B), 10)
+	i := o.A
+	if i == 0 {
+		i = 1
+	}
+	a := strconv.FormatUint(uint64(i), 10)
+	return "rgba(" + r + "," + g + "," + b + "," + a + ")"
+}
+
+func (o Object) formatRGB() string {
+	r := strconv.FormatUint(uint64(o.R), 10)
+	g := strconv.FormatUint(uint64(o.G), 10)
+	b := strconv.FormatUint(uint64(o.B), 10)
+	return "rgb(" + r + "," + g + "," + b + ")"
+}
---
+++ colors/object_test.go
@@ -0,0 +1,43 @@
+package colors
+
+import "testing"
+
+func TestObject(t *testing.T) {
+	o, err := FromHex("#5f55f5")
+	if err != nil {
+		t.Fatal(err)
+	}
+	e := "rgb(95,85,245)"
+	g := o.String()
+	if g != e {
+		t.Errorf("expected %s got %s", e, g)
+	}
+
+	o.IsRGBA = true
+	e = "rgba(95,85,245,1)"
+	g = o.String()
+	if g != e {
+		t.Errorf("expected %s got %s", e, g)
+	}
+}
+
+func TestSubStr(t *testing.T) {
+	sample := []struct {
+		src           string
+		start, length int
+		exp           string
+	}{
+		{"#5f55f5", 1, 2, "5f"},
+		{"#5f55f5", 3, 2, "55"},
+		{"#5f55f5", 5, 2, "f5"},
+	}
+	for _, e := range sample {
+		g, err := substrc(e.src, e.start, e.length)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if g != e.exp {
+			t.Errorf("expected %s got %s", e.exp, g)
+		}
+	}
+}
